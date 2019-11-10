2339dddaf1b00d7b75300b84aa2b9624279f4a44
fingoldin <vassilioskaxiras@gmail.com>
2019-11-10 00:56:40 -0500

    add recent

--- package/stocks/stocks.py
+++ package/stocks/stocks.py
@@ -1,8 +1,17 @@
 from .sql import SQL
+from .utils import utc_timestamp
 
 def get_options(stock):
   data = []
   with SQL() as c:
     data = c.get("SELECT * FROM options WHERE stock IN (SELECT id from stocks where name=\"" + str(stock) + "\")")
+
+  return data
+
+# time is in minutes 
+def get_recent_options(time=100):
+  data = []
+  with SQL() as c:
+    data = c.get("SELECT * FROM options WHERE timestamp>\"" + str(utc_timestamp() - time * 60) + "\"")
 
   return data
