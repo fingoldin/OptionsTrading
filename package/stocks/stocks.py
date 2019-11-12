f75ac8f34ebf1047164cea6d857b899c2f81fd3c
Rajat Mittal <rajat0502@gmail.com>
2019-11-12 11:50:28 -0500

    Added get stock name function

--- package/stocks/stocks.py
+++ package/stocks/stocks.py
@@ -8,10 +8,19 @@ def get_options(stock):
 
   return data
 
-# time is in minutes 
+# time is in minutes
 def get_recent_options(time=100):
   data = []
   with SQL() as c:
     data = c.get("SELECT * FROM options WHERE timestamp>\"" + str(utc_timestamp() - time * 60) + "\"")
 
   return data
+
+def get_stock_name(id):
+    name = ""
+    with SQL() as c:
+        name = c.get("SELECT * from stocks WHERE id=\"" + str(id) + "\"")
+    if (len(name) > 0):
+        return name[0]["name"]
+    else:
+        return ""
